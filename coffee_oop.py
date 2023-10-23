def77e109bee85a754c4519f35f97a02dea1af4c
demiapollo <apollodemirel67@gmail.com>
2023-10-22 20:44:50 -0700

    coffee machine oop completed.

---
+++ coffee_oop.py
@@ -0,0 +1,20 @@
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Picachu", "Squirtle", "Charmandler"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# table.align = "l"
+# print(table)
+
+
